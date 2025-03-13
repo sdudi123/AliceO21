ade69024341c6b28c625968a12b69769587dd879
David Rohr <drohr@jwdt.org>
2025-03-13 15:00:35 +0100

    GPU Standalone: fix prepare script using O2 env modules

--- GPU/GPUTracking/Standalone/cmake/prepare.sh
+++ GPU/GPUTracking/Standalone/cmake/prepare.sh
@@ -10,7 +10,7 @@ else
   WORK_DIR="$ALIBUILD_WORK_DIR"
 fi
 eval "`alienv shell-helper`"
-alienv load O2/latest
-for i in Vc boost fmt CMake ms_gsl Clang ninja; do
+# alienv load O2/latest
+for i in Vc boost fmt CMake ms_gsl Clang ninja TBB ROOT; do
   source sw/$ALIARCH/$i/latest/etc/profile.d/init.sh
 done
