21ec6c2620ee91b213e03b9c15132ca524fa4ff7
Francesco Mazzaschi <fmazzasc@alipap1.cern.ch>
2025-04-23 10:50:52 +0200

    Fix loop + copyright

--- Generators/include/Generators/NuclCoalescencePythia8.h
+++ Generators/include/Generators/NuclCoalescencePythia8.h
@@ -1,3 +1,14 @@
+// Copyright 2019-2020 CERN and copyright holders of ALICE O2.
+// See https://alice-o2.web.cern.ch/copyright for details of the copyright holders.
+// All rights not expressly granted are reserved.
+//
+// This software is distributed under the terms of the GNU General Public
+// License v3 (GPL Version 3), copied verbatim in the file "COPYING".
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
 #ifndef O2_COALESCENCE_H
 #define O2_COALESCENCE_H
 
@@ -73,10 +84,9 @@ bool coalPythia8(Pythia8::Event& event, int charge, int pdgCode, float mass, dou
 bool CoalAfterburner(Pythia8::Event& event, std::vector<unsigned int> inputPdgList = {}, double coalMomentum = 0.4, int firstDauID = -1, int lastDauId = -1)
 {
   const double coalescenceRadius{0.5 * 1.122462 * coalMomentum};
-
   // if coalescence from a heavy hadron, loop only between firstDauID and lastDauID
   int loopStart = firstDauID > 0 ? firstDauID : 0;
-  int loopEnd = lastDauId > 0 ? lastDauId : event.size();
+  int loopEnd = lastDauId > 0 ? lastDauId : event.size() - 1;
   // fill the nuclear mask
   uint8_t nuclearMask = 0;
   for (auto nuclPdg : inputPdgList) {
@@ -97,7 +107,7 @@ bool CoalAfterburner(Pythia8::Event& event, std::vector<unsigned int> inputPdgLi
   }
   // fill nucleon pools
   std::vector<int> protons[2], neutrons[2], lambdas[2];
-  for (auto iPart{loopStart}; iPart < loopEnd; ++iPart) {
+  for (auto iPart{loopStart}; iPart <= loopEnd; ++iPart) {
     if (std::abs(event[iPart].y()) > 1.) // skip particles with y > 1
     {
       continue;
