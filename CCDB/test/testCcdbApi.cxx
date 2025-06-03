02d68188d79ae96009b8bb21c1d3faf6e6d7632d
Barthelemy <barthelemy.von.haller@cern.ch>
2025-06-03 09:33:17 +0200

    [O2-5887] Detect if an empty URL is passed to the CCDB api.

--- CCDB/test/testCcdbApi.cxx
+++ CCDB/test/testCcdbApi.cxx
@@ -590,3 +590,10 @@ BOOST_AUTO_TEST_CASE(vectored)
     BOOST_CHECK(context.dest.size() != 0);
   }
 }
+
+BOOST_AUTO_TEST_CASE(empty_url)
+{
+  CcdbApi api;
+  ccdbUrl = "";
+  BOOST_CHECK_EXCEPTION(api.init(ccdbUrl), invalid_argument, [](std::invalid_argument const&) -> bool { return true; });
+}
