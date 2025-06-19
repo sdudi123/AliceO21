5d41f7639b2d653fd2ba37a9e55b7be4907befa8
ChunzhengLab <chunzheng.wang@icloud.com>
2025-06-19 22:33:42 +0200

    should use the total eff but not only seed

--- Detectors/Upgrades/ITS3/macros/test/CheckChipResponseFile.C
+++ Detectors/Upgrades/ITS3/macros/test/CheckChipResponseFile.C
@@ -57,25 +57,25 @@ void LoadRespFunc()
   std::string AptsFile = "$(O2_ROOT)/share/Detectors/Upgrades/ITS3/data/ITS3ChipResponseData/APTSResponseData.root";
   std::string AlpideFile = "$(O2_ROOT)/share/Detectors/ITSMFT/data/AlpideResponseData/AlpideResponseData.root";
 
-  std::cout<<"=====================\n";
+  std::cout << "=====================\n";
   LOGP(info, "ALPIDE Vbb=0V response");
   mAlpSimResp0 = loadResponse(AlpideFile, "response0"); // Vbb=0V
   mAlpSimResp0->computeCentreFromData();
   mAlpSimResp0->print();
   LOGP(info, "Response Centre {}", mAlpSimResp0->getRespCentreDep());
-  std::cout<<"=====================\n";
+  std::cout << "=====================\n";
   LOGP(info, "ALPIDE Vbb=-3V response");
   mAlpSimResp1 = loadResponse(AlpideFile, "response1"); // Vbb=-3V
   mAlpSimResp1->computeCentreFromData();
   mAlpSimResp1->print();
   LOGP(info, "Response Centre {}", mAlpSimResp1->getRespCentreDep());
-  std::cout<<"=====================\n";
+  std::cout << "=====================\n";
   LOGP(info, "APTS response");
   mAptSimResp1 = loadResponse(AptsFile, "response1"); // APTS
   mAptSimResp1->computeCentreFromData();
   mAptSimResp1->print();
   LOGP(info, "Response Centre {}", mAptSimResp1->getRespCentreDep());
-  std::cout<<"=====================\n";
+  std::cout << "=====================\n";
 }
 
 std::vector<float> getCollectionSeediciencies(o2::its3::ChipSimResponse* resp,
@@ -85,7 +85,7 @@ std::vector<float> getCollectionSeediciencies(o2::its3::ChipSimResponse* resp,
   bool flipRow = false, flipCol = false;
   for (auto depth : depths) {
     auto rspmat = resp->getResponse(0.0, 0.0,
-                                    um2cm(depth) + resp->getDepthMin() + 1.e-9,
+                                    um2cm(depth) + 1.e-9,
                                     flipRow, flipCol);
     seed.push_back(rspmat ? rspmat->getValue(2, 2) : 0.f);
   }
@@ -99,7 +99,7 @@ std::vector<float> getShareValues(o2::its3::ChipSimResponse* resp,
   bool flipRow = false, flipCol = false;
   for (auto depth : depths) {
     auto rspmat = resp->getResponse(0.0, 0.0,
-                                    um2cm(depth) + resp->getDepthMin() + 1.e-9,
+                                    um2cm(depth) + 1.e-9,
                                     flipRow, flipCol);
     float s = 0;
     int npix = resp->getNPix();
@@ -121,7 +121,7 @@ std::vector<float> getEffValues(o2::its3::ChipSimResponse* resp,
   bool flipRow = false, flipCol = false;
   for (auto depth : depths) {
     auto rspmat = resp->getResponse(0.0, 0.0,
-                                    um2cm(depth) + resp->getDepthMin() + 1.e-9,
+                                    um2cm(depth) + 1.e-9,
                                     flipRow, flipCol);
     float s = 0;
     int npix = resp->getNPix();
@@ -140,9 +140,12 @@ void CheckChipResponseFile()
   LoadRespFunc();
   LOG(info) << "Response function loaded" << std::endl;
 
-  std::vector<float> vecDepth(50);
-  for (int i = 0; i < 50; ++i)
-    vecDepth[i] = i;
+  std::vector<float> vecDepth;
+  int numPoints = 100;
+  for (int i = 0; i < numPoints; ++i) {
+    float value = -50 + i * (100.0f / (numPoints - 1));
+    vecDepth.push_back(value);
+  }
 
   int colors[] = {kOrange + 7, kRed + 1, kAzure + 4};
   struct RespInfo {
@@ -156,7 +159,7 @@ void CheckChipResponseFile()
     {mAlpSimResp1, "ALPIDE Vbb=-3V", colors[2]}};
 
   TCanvas* c1 = new TCanvas("c1", "c1", 800, 600);
-  TH1* frame = c1->DrawFrame(-1, -0.049, 50, 1.049);
+  TH1* frame = c1->DrawFrame(-50, -0.049, 50, 1.049);
   frame->SetTitle(";Depth(um);Charge Collection Seed / Share / Eff");
   TLegend* leg = new TLegend(0.15, 0.5, 0.4, 0.85);
   leg->SetFillStyle(0);
