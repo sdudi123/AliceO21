be6caa5060cd0e4c2f24e32396bd51fe46a1c629
Felix Schlepper <felix.schlepper@cern.ch>
2025-07-18 11:47:01 +0200

    ITS: truth seeding fill labels
    Signed-off-by: Felix Schlepper <felix.schlepper@cern.ch>

--- Detectors/ITSMFT/ITS/tracking/src/VertexerTraits.cxx
+++ Detectors/ITSMFT/ITS/tracking/src/VertexerTraits.cxx
@@ -575,7 +575,12 @@ void VertexerTraits::addTruthSeedingVertices()
   int64_t roFrameBiasInBC = o2::itsmft::DPLAlpideParam<o2::detectors::DetID::ITS>::Instance().roFrameBiasInBC;
   int64_t roFrameLengthInBC = o2::itsmft::DPLAlpideParam<o2::detectors::DetID::ITS>::Instance().roFrameLengthInBC;
   o2::steer::MCKinematicsReader mcReader(dc);
-  std::map<int, bounded_vector<Vertex>> vertices;
+  struct VertInfo {
+    bounded_vector<Vertex> vertices;
+    bounded_vector<int> srcs;
+    bounded_vector<int> events;
+  };
+  std::map<int, VertInfo> vertices;
   for (int iSrc{0}; iSrc < mcReader.getNSources(); ++iSrc) {
     auto eveId2colId = dc->getCollisionIndicesForSource(iSrc);
     for (int iEve{0}; iEve < mcReader.getNEvents(iSrc); ++iEve) {
@@ -584,7 +589,11 @@ void VertexerTraits::addTruthSeedingVertices()
         const auto& eve = mcReader.getMCEventHeader(iSrc, iEve);
         int rofId = ((ir - raw::HBFUtils::Instance().getFirstSampledTFIR()).toLong() - roFrameBiasInBC) / roFrameLengthInBC;
         if (!vertices.contains(rofId)) {
-          vertices[rofId] = bounded_vector<Vertex>(mMemoryPool.get());
+          vertices[rofId] = {
+            .vertices = bounded_vector<Vertex>(mMemoryPool.get()),
+            .srcs = bounded_vector<int>(mMemoryPool.get()),
+            .events = bounded_vector<int>(mMemoryPool.get()),
+          };
         }
         Vertex vert;
         vert.setTimeStamp(rofId);
@@ -595,7 +604,9 @@ void VertexerTraits::addTruthSeedingVertices()
         vert.setChi2(1);
         constexpr float cov = 50e-9;
         vert.setCov(cov, cov, cov, cov, cov, cov);
-        vertices[rofId].push_back(vert);
+        vertices[rofId].vertices.push_back(vert);
+        vertices[rofId].srcs.push_back(iSrc);
+        vertices[rofId].events.push_back(iEve);
       }
     }
   }
@@ -604,10 +615,11 @@ void VertexerTraits::addTruthSeedingVertices()
     bounded_vector<Vertex> verts(mMemoryPool.get());
     bounded_vector<std::pair<o2::MCCompLabel, float>> polls(mMemoryPool.get());
     if (vertices.contains(iROF)) {
-      verts = vertices[iROF];
+      const auto& vertInfo = vertices[iROF];
+      verts = vertInfo.vertices;
       nVerts += verts.size();
       for (size_t i{0}; i < verts.size(); ++i) {
-        o2::MCCompLabel lbl; // unset label for now
+        o2::MCCompLabel lbl(o2::MCCompLabel::maxTrackID(), vertInfo.events[i], vertInfo.srcs[i], false);
         polls.emplace_back(lbl, 1.f);
       }
     } else {
